910abe6182891ddc4c018ec4f5f011042bfeaea7
Jordan Ehrlich <jehrlich@harriscomputer.com>
2017-01-30 17:03:26 -0600

    Refactor middleware

--- server.js
+++ server.js
@@ -2,17 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-	requireAuthenication: function(req, res, next) {
-		console.log('private route hit!');
-		next();
-	},
-	logger: function(req, res, next) {
-		console.log('Request: ' + new Date().toString() + ': ' +
-			req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
+var middleware = require('./middleware');
 
 // app.use(middleware.requireAuthenication);
 app.use(middleware.logger);
